974373df4ae5f66c2d5b3ac11ac81688ef8cced1
Alina Grosu <alinagrosu@ex.ua>
2018-04-18 13:51:50 +0300

    csms-frwk - some junk just in case

---
+++ csms-frk/src/test/resources/com/ss/cuketest/junk/Propps.java
@@ -0,0 +1,9 @@
+package com.cosmos.webdriver.config.impl;
+
+interface Propps {
+	String EXECUTION_TYPE_ENVIRO_KEY = "com.cosmos.execution.type";
+	String BROWSER_ENVIRO_KEY = "com.cosmos.driver.browser";
+	String GRID_URL_KEY = "com.cosmos.grid.hub.url";
+	String CHROMEDRIVER_LOCATION_KEY = "com.cosmos.driver.chrome.executable.location";
+	String STEPS_CONTEXT_SCOPE_KEY = "com.cosmos.steps.context.scope";
+}
